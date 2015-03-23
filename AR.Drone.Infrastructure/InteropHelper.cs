6edc1640ceb49f39def48b8a3ae039b8c20e2d00
yss930819 <yss930819@163.com>
2015-03-23 17:07:09 +0800

    阅读完入口点函数
    阅读入口代码

--- AR.Drone.Infrastructure/InteropHelper.cs
+++ AR.Drone.Infrastructure/InteropHelper.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * 时间 2015 3 23
+ * 注释 杨率帅
+ *
+ * 本类为辅助工具类
+ * 用来设置FFmpeg库的准确位置
+ */
+
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -6,7 +14,7 @@ namespace AR.Drone.Infrastructure
 {
     public static class InteropHelper
     {
-        public const string LD_LIBRARY_PATH = "LD_LIBRARY_PATH";
+        public const string LD_LIBRARY_PATH = "LD_LIBRARY_PATH"; //字符串常量
 
         public static void RegisterLibrariesSearchPath(string path)
         {
@@ -26,7 +34,13 @@ namespace AR.Drone.Infrastructure
             }
         }
 
+        
         [DllImport("kernel32", SetLastError = true)]
+        /// <summary>
+        /// 调用系统本身的动态链接库，设置本程序需要调用的动态链接库
+        /// </summary>
+        /// <param name="lpPathName">要设置的链接库的地址</param>
+        /// <returns></returns>
         public static extern bool SetDllDirectory(string lpPathName);
     }
 }
